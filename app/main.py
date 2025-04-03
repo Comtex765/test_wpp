b95f6b072ca12f0127b387c79754c49f4eee0dcc
Fernando Novillo <70916955+Comtex765@users.noreply.github.com>
2025-04-02 21:49:51 -0500

    bugs fixxed

--- app/main.py
+++ app/main.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hola_mundo():
 
-
-    
-    return render_template('holaflask.html')
-    
+    return render_template("holaflask.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=1111)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
