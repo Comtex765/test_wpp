7e575c3e2af1d8059dc5a5d0263d3c4e800e9ddb
Fernando Novillo <70916955+Comtex765@users.noreply.github.com>
2025-04-02 21:37:31 -0500

    minor changes

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hola_mundo():
     return render_template('holaflask.html')
+    
 
+    
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=1111)
